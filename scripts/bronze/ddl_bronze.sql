61f5d9f87a1aa78f29351c5377ba490da2b91be2
Shraban Kumar Naik <shraban020@gmail.com>
2026-02-16 11:38:48 +0530

    Fix column definition formatting in ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -52,7 +52,7 @@ IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
 	DROP TABLE bronze.erp_loc_a101;
 CREATE TABLE bronze.erp_loc_a101 (
 cid		NVARCHAR(50),
-cntry	NVARCHAR(50),
+cntry	NVARCHAR(50)
 );
 
 IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
@@ -60,7 +60,7 @@ IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
 CREATE TABLE bronze.erp_cust_az12 (
 cid		NVARCHAR(50),
 bdate	DATE,
-gen		NVARCHAR(50),
+gen		NVARCHAR(50)
 );
 
 IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
