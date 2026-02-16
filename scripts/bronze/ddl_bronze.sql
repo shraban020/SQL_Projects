69aac5d45a46628867cde2f7c4eca2224d4a3fb1
Shraban Kumar Naik <shraban020@gmail.com>
2026-02-16 16:50:40 +0530

    Update ddl_bronze.sql

--- scripts/bronze/ddl_bronze.sql
+++ scripts/bronze/ddl_bronze.sql
@@ -68,6 +68,6 @@ IF OBJECT_ID ('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
 CREATE TABLE bronze.erp_px_cat_g1v2 (
 id			NVARCHAR(50),
 cat			NVARCHAR(50),
-sucat		NVARCHAR(50),
+subcat		NVARCHAR(50),
 maintenance NVARCHAR(50)
 );
